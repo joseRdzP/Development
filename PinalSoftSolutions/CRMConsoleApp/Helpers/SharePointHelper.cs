a460d82fff221285ed0aa811b2ecd7f8498c91c7
Alberto Rodriguez <arodriguez@opentechalliance.com>
2021-06-17 20:56:38 -0500

    Working on SharePoint Calls

--- PinalSoftSolutions/CRMConsoleApp/Helpers/SharePointHelper.cs
+++ PinalSoftSolutions/CRMConsoleApp/Helpers/SharePointHelper.cs
@@ -6,14 +6,21 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using CRMConsoleApp.Common;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.IO;
+using CRMConsoleApp.Models;
+using System.Net;
+using System.Web.Script.Serialization;
 
 namespace CRMConsoleApp.Helpers
 {
     public class SharePointHelper
     {
+        const string msoStsUrl = "https://login.microsoftonline.com/extSTS.srf";
+        const string msoHrdUrl = "https://login.microsoftonline.com/GetUserRealm.srf";
+        const string spowssigninUri = "_forms/default.aspx?wa=wsignin1.0";
+        const string contextInfoQuery = "_api/contextinfo";
+
         public static async Task<string> GetAccessToken()
         {
             SharePointResponse sharePointResponse = new SharePointResponse();
@@ -47,21 +54,43 @@ namespace CRMConsoleApp.Helpers
             return accessToken;
         }
 
-        [DataContract]
-        public class SharePointResponse
+        public static void CreateFolder(string siteUrl, string relativePath)
         {
-            [DataMember]
-            public string token_type { get; set; }
-            [DataMember]
-            public string expires_in { get; set; }
-            [DataMember]
-            public string not_before { get; set; }
-            [DataMember]
-            public string expires_on { get; set; }
-            [DataMember]
-            public string resource { get; set; }
-            [DataMember]
-            public string access_token { get; set; }
+            var odataQuery = "_api/web/folders";
+            var contentToPost = @"{ '__metadata': { 'type': 'SP.Folder' }, 'ServerRelativeUrl': '" + relativePath + "'}";
+            byte[] content = Encoding.UTF8.GetBytes(contentToPost);
+            var url = new Uri(string.Format("{0}/{1}", siteUrl, odataQuery));
+            var webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add("X-RequestDigest", GetFormDigest());
+
+        }
+
+        public static string GetFormDigest()
+        {
+            string formDigest = null;
+
+            string resourceUrl = "http://basesmc15/_api/contextinfo";
+            HttpWebRequest wreq = WebRequest.Create(resourceUrl) as HttpWebRequest;
+            wreq.UseDefaultCredentials = true;
+            wreq.Method = "POST";
+            wreq.Accept = "application/json;odata=verbose";
+            wreq.ContentLength = 0;
+            wreq.ContentType = "application/json";
+            string result;
+            WebResponse wresp = wreq.GetResponse();
+
+            using (StreamReader sr = new StreamReader(wresp.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            var jss = new JavaScriptSerializer();
+            var val = jss.Deserialize<Dictionary<string, object>>(result);
+            var d = val["d"] as Dictionary<string, object>;
+            var wi = d["GetContextWebInformation"] as Dictionary<string, object>;
+            formDigest = wi["FormDigestValue"].ToString();
+
+            return formDigest;
         }
     }
 }
---
+++ PinalSoftSolutions/CRMConsoleApp/Models/SharePointResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace CRMConsoleApp.Models
+{
+    [DataContract]
+    public class SharePointResponse
+    {
+        [DataMember]
+        public string token_type { get; set; }
+        [DataMember]
+        public string expires_in { get; set; }
+        [DataMember]
+        public string not_before { get; set; }
+        [DataMember]
+        public string expires_on { get; set; }
+        [DataMember]
+        public string resource { get; set; }
+        [DataMember]
+        public string access_token { get; set; }
+    }
+}
