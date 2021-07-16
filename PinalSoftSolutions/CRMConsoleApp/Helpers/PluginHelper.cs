03eba73a4861ffbb9a7fe7642c58c49b40546aba
Alberto Rodriguez <arodriguez@opentechalliance.com>
2021-07-15 22:27:49 -0500

    Deserialization changed to use Models

--- PinalSoftSolutions/CRMConsoleApp/Helpers/PluginHelper.cs
+++ PinalSoftSolutions/CRMConsoleApp/Helpers/PluginHelper.cs
@@ -1,6 +1,20 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using zCRMConsoleApp.Models;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using System.Runtime.Serialization;
+using System.Reflection;
 
 namespace zCRMConsoleApp.Helpers
 {
@@ -56,5 +70,98 @@ namespace zCRMConsoleApp.Helpers
                 }
             }
         }
-    }
+
+        public static void RetrieveMultipleResultsForView(string legacyDataUrl)
+        {
+			try
+			{
+				EntityCollection collection = new EntityCollection();
+				Console.WriteLine("RetrieveMultiplePlugin : Retrieving Facility DIDs...");
+				try
+				{
+					using (HttpClient client = new HttpClient())
+					{
+						client.Timeout = TimeSpan.FromMilliseconds(15000);
+						client.DefaultRequestHeaders.ConnectionClose = true;
+						client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+						client.DefaultRequestHeaders.Add("AccessToken", "095c1d5b-545c-4891-8ab2-c02057653689");
+						var response = client.GetAsync(legacyDataUrl).Result;
+						response.EnsureSuccessStatusCode();
+
+						var responseText = response.Content.ReadAsStringAsync().Result;
+						
+						if (!string.IsNullOrEmpty(responseText))
+                        {
+							Console.WriteLine("RetrieveMultiplePlugin : Facility DIDs Retrieved!");
+
+							Console.WriteLine("RetrieveMultiplePlugin : Deserializing Facility DIDs Response...");
+							JObject facilityDIDJObject = JObject.Parse(responseText);
+
+							if (facilityDIDJObject != null)
+                            {
+								JToken facilityDIDJToken = facilityDIDJObject["value"];
+
+								if (facilityDIDJToken != null)
+                                {
+									List<FacilityDID> facilityDIDs = JsonConvert.DeserializeObject<List<FacilityDID>>(facilityDIDJToken.ToString());
+									Console.WriteLine("Number of FacilityDIDs = {0}", facilityDIDs.Count);
+
+									if (facilityDIDs.Count > 0)
+                                    {
+										foreach (FacilityDID facilityDID in facilityDIDs)
+                                        {
+											Entity e = new Entity("ota_legacyfacilitydid");
+
+											Dictionary<string, string> facilityDIDDictionary = JObject.FromObject(facilityDID).ToObject<Dictionary<string, string>>();
+
+											if (facilityDIDDictionary.Count > 0)
+											{
+												foreach (dynamic keyValuePair in facilityDIDDictionary)
+												{
+													string key = keyValuePair.Key;
+													object val = keyValuePair.Value;
+													Console.WriteLine("=====> Processing Column Name : {0} with Value = {1}", key, val);
+													e.Attributes[key] = val;
+												}
+												collection.Entities.Add(e);
+											}
+											//Type fieldsType = typeof(FacilityDID);
+
+											//PropertyInfo[] props = fieldsType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+											//for (int i = 0; i < props.Length; i++)
+											//{
+
+											//}
+										}
+									}
+								}					
+							}						
+						}									
+
+						//Console.WriteLine("RetrieveMultiplePlugin : Sending the Entity Collection to the Output Parameters...");
+						//context.OutputParameters["BusinessEntityCollection"] = collection;
+						//Console.WriteLine("RetrieveMultiplePlugin : Output Parameters operation is Done.");
+						//throw new InvalidPluginExecutionException("Ticket Sources Retrieved.");
+					}
+				}
+				catch (AggregateException aex)
+				{
+					Console.WriteLine("Inner Exceptions: ");
+					foreach (Exception ex in aex.InnerExceptions)
+					{
+						Console.WriteLine("Exception: {0}", ex.ToString());
+					}
+					throw new InvalidPluginExecutionException(string.Format(CultureInfo.InvariantCulture,
+						"An exception occurred while retrieving Ticket Sources.", aex));
+				}
+				Console.WriteLine("Collection Items = {0}", collection.Entities.Count);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception: {0}", e.ToString());
+				throw;
+			}
+		}
+	}
 }
--- PinalSoftSolutions/CRMConsoleApp/Program.cs
+++ PinalSoftSolutions/CRMConsoleApp/Program.cs
@@ -12,68 +12,11 @@ namespace zCRMConsoleApp
             try
             {
                 //Call OData Legacy API
-                string apiUrl = "https://virtualcruds20210713172404.azurewebsites.net/api/values";
+                //string apiUrl = "https://virtualcruds20210713172404.azurewebsites.net/api/values";
                 //PluginHelper.GetAccountData();
-                PluginHelper.GetAccountDataAzure(apiUrl);
+                //PluginHelper.GetAccountDataAzure(apiUrl);
 
-                /*
-                //Get SP Configurations
-                string spClientId = SharePointCredentials.ClientId;
-                string spClientSecret = SharePointCredentials.ClientSecret;
-                string spRealm = SharePointCredentials.Realm;
-                string spPrincipal = SharePointCredentials.Principal;
-                string spTargetHost = SharePointCredentials.TargetHost;
-                string spSiteUrl = $"https://{spTargetHost}/sites/{SharePointCredentials.SPSiteName}";
-                string spUri = SharePointCredentials.Uri;
-
-                //Create Folder Params//////////////////
-                string folderName = "Test 001 Alberto-123456";
-                string extraFolders = "Order Agreements & Amendments,Quotes & Proposals,Termination Agreements";
-                ////////////////////////////////////////
-
-                //Rename Folder Params///////////////////////
-                string entityName = "account";
-                string oldRelativePath = "01 Alberto-567890";
-                string newFolderName = "03 Alberto-333333";
-                /////////////////////////////////////////////
-
-                Console.WriteLine("------------------------------------------------------------------------------------------");
-
-                //Getting the SP Access Token
-                string spAccessToken = SharePointHelper.GetSpAccessToken(spClientId, spRealm, spClientSecret, spPrincipal, spTargetHost, spUri).Result;
-
-                if (!string.IsNullOrEmpty(spAccessToken))
-                {
-                    //Getting the SharePoint Service instance
-                    SharePointService spService = new SharePointService(spAccessToken, spSiteUrl);
-
-                    if (spService != null)
-                    {
-                        //Create Folder
-                        Console.WriteLine("Creating the Main Folder : " + $"{entityName}/{folderName}");
-                        string creationResponse = spService.CreateFolder($"{entityName}/{folderName}");
-                        Console.WriteLine("Create Folder Response : " + creationResponse);
-                        Console.WriteLine("------------------------------------------------------------------------------------------");
-
-                        //Create Extra Folders
-                        Console.WriteLine("Creating Sub-Folders...");
-                        var folderList = extraFolders.Split(',');
-                        for (int i = 0; i < folderList.Length; i++)
-                        {
-                            string currentExtraFolderName = folderList[i];
-                            Console.WriteLine("- Sub-Folder #" + i + " - " + $"{entityName}/{folderName}/{currentExtraFolderName}");
-                            string extraFolderCreationResponse = spService.CreateFolder($"{entityName}/{folderName}/{currentExtraFolderName}");
-                            Console.WriteLine("Create Extra Folder Response #" + i + " - " + extraFolderCreationResponse);
-                            Console.WriteLine("------------------------------------------------------------------------------------------");
-                        }
-                        
-                        //Rename Folder
-                        string renameResponse = spService.RenameFolder($"{entityName}/{oldRelativePath}", newFolderName);
-                        Console.WriteLine("Rename Folder Response : " + renameResponse);
-                        Console.WriteLine("------------------------------------------------------------------------------------------");
-                    }
-                }
-                */
+                PluginHelper.RetrieveMultipleResultsForView("https://accountmgt.opentechalliance.com/odata/FacilityDID?$top=100");
             }
             catch (Exception ex)
             {
@@ -83,6 +26,66 @@ namespace zCRMConsoleApp
             {
                 Console.WriteLine("Press Enter to Exit from the Console App...");
                 Console.ReadKey();
+            }
+        }
+
+        private static void SharePointLogic()
+        {
+            //Get SP Configurations
+            string spClientId = SharePointCredentials.ClientId;
+            string spClientSecret = SharePointCredentials.ClientSecret;
+            string spRealm = SharePointCredentials.Realm;
+            string spPrincipal = SharePointCredentials.Principal;
+            string spTargetHost = SharePointCredentials.TargetHost;
+            string spSiteUrl = $"https://{spTargetHost}/sites/{SharePointCredentials.SPSiteName}";
+            string spUri = SharePointCredentials.Uri;
+
+            //Create Folder Params//////////////////
+            string folderName = "Test 001 Alberto-123456";
+            string extraFolders = "Order Agreements & Amendments,Quotes & Proposals,Termination Agreements";
+            ////////////////////////////////////////
+
+            //Rename Folder Params///////////////////////
+            string entityName = "account";
+            string oldRelativePath = "01 Alberto-567890";
+            string newFolderName = "03 Alberto-333333";
+            /////////////////////////////////////////////
+
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+
+            //Getting the SP Access Token
+            string spAccessToken = SharePointHelper.GetSpAccessToken(spClientId, spRealm, spClientSecret, spPrincipal, spTargetHost, spUri).Result;
+
+            if (!string.IsNullOrEmpty(spAccessToken))
+            {
+                //Getting the SharePoint Service instance
+                SharePointService spService = new SharePointService(spAccessToken, spSiteUrl);
+
+                if (spService != null)
+                {
+                    //Create Folder
+                    Console.WriteLine("Creating the Main Folder : " + $"{entityName}/{folderName}");
+                    string creationResponse = spService.CreateFolder($"{entityName}/{folderName}");
+                    Console.WriteLine("Create Folder Response : " + creationResponse);
+                    Console.WriteLine("------------------------------------------------------------------------------------------");
+
+                    //Create Extra Folders
+                    Console.WriteLine("Creating Sub-Folders...");
+                    var folderList = extraFolders.Split(',');
+                    for (int i = 0; i < folderList.Length; i++)
+                    {
+                        string currentExtraFolderName = folderList[i];
+                        Console.WriteLine("- Sub-Folder #" + i + " - " + $"{entityName}/{folderName}/{currentExtraFolderName}");
+                        string extraFolderCreationResponse = spService.CreateFolder($"{entityName}/{folderName}/{currentExtraFolderName}");
+                        Console.WriteLine("Create Extra Folder Response #" + i + " - " + extraFolderCreationResponse);
+                        Console.WriteLine("------------------------------------------------------------------------------------------");
+                    }
+                        
+                    //Rename Folder
+                    string renameResponse = spService.RenameFolder($"{entityName}/{oldRelativePath}", newFolderName);
+                    Console.WriteLine("Rename Folder Response : " + renameResponse);
+                    Console.WriteLine("------------------------------------------------------------------------------------------");
+                }
             }
         }
     }
